b020d3eb0d77106b24fb0f788703742b0658325f
pcb <pcb@unit.no>
2020-08-22 00:46:27 +0200

    added login

--- src/App.js
+++ src/App.js
@@ -1,83 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import firebase from "./firebase";
-import "firebase/firestore";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Login";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import MainPage from "./MainPage";
 
-function App() {
-  const [itemTitle, setItemTitle] = useState("");
-  const [items, setItems] = useState([
-    { itemTitle: "item1" },
-    { itemTitle: "item2" },
-    { itemTitle: "item3" },
-  ]);
-
-  useEffect(() => {
-    // firebase.firestore().collection("users")
-    // .add({
-    //   first: "PCB",
-    //   last: "Lovelace",
-    //   born: 1815,
-    // })
-    // .then(function (docRef) {
-    //   console.log("Document written with ID: ", docRef.id);
-    // })
-    // .catch(function (error) {
-    //   console.error("Error adding document: ", error);
-    // });
-
-    // firebase
-    //   .firestore()
-    //   .collection("todos")
-    //   //.where("country", "==", "USA")
-    //   .get()
-    //   .then((querySnapshot) => {
-    //     const todoItems = querySnapshot.docs.map((doc) => doc.data());
-    //     setItems(todoItems);
-    //     querySnapshot.forEach((doc) => {
-    //       console.log(`${doc.data().itemTitle}`);
-    //     });
-    //   });
-  }, []);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    //setItems(itemTitle)
-    let pcb = {"itemTitle":itemTitle};
-
-    setItems([...items, pcb]);
-    // firebase
-    //   .firestore()
-    //   .collection("todos")
-    //   .add({
-    //     itemTitle,
-    //   })
-    //   .then(function (docRef) {
-    //     console.log("Document written with ID: ", docRef.id);
-    //   })
-    //   .catch(function (error) {
-    //     console.error("Error adding document: ", error);
-    //   });
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>TODO</h1>
-      <pre>{items.size}</pre>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label></label>
-        </div>
-        <input
-          type="text"
-          value={itemTitle}
-          onChange={(e) => setItemTitle(e.currentTarget.value)}
-        />
-        <button>Add</button>
-      </form>
-      {items &&
-        items.map((item, index) => <li key={index}>{item.itemTitle}</li>)}
-    </div>
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRoute exact path="/" component={MainPage} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </AuthProvider>
   );
-}
+};
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,39 +1,15 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlc-9cBQVLLXdLxYfVpgh8I3iFALoQ7E0",
-  authDomain: "todo-2ac34.firebaseapp.com",
-  databaseURL: "https://todo-2ac34.firebaseio.com",
-  projectId: "todo-2ac34",
-  storageBucket: "todo-2ac34.appspot.com",
-  messagingSenderId: "144767486267",
-  appId: "1:144767486267:web:53c6a77d36c83e42db4f1e",
+    apiKey: "AIzaSyBlc-9cBQVLLXdLxYfVpgh8I3iFALoQ7E0",
+    authDomain: "todo-2ac34.firebaseapp.com",
+    databaseURL: "https://todo-2ac34.firebaseio.com",
+    projectId: "todo-2ac34",
+    storageBucket: "todo-2ac34.appspot.com",
+    messagingSenderId: "144767486267",
+    appId: "1:144767486267:web:53c6a77d36c83e42db4f1e",
 };
 
-//if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-
-  /*
-  firebase.firestore().collection("users")
-    .add({
-      first: "PCB",
-      last: "Lovelace",
-      born: 1815,
-    })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
-     });
-
-  db.collection("users")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.data().first} ${doc.data().last}`);
-      });
-    }); */
-//}
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
