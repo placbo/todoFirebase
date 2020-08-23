fe2181b002ee3a873d47b641709c522de2ea0b67
pcb <pcb@unit.no>
2020-08-23 23:37:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf9531c24234585f3ae900f8d9af0ddb.js"
+  "/precache-manifest.a02656876226ac37575476b1f535a256.js"
 );
 
 self.addEventListener('message', (event) => {
