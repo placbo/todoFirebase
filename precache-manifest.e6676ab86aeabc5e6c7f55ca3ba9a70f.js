9a6fcf8f1a831c4baa1889961b71e13fb3d436c8
pcb <pcb@unit.no>
2020-08-24 23:13:03 +0200

    Updates

---
+++ precache-manifest.e6676ab86aeabc5e6c7f55ca3ba9a70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2dd18e3bca6caf651b32aa910cd02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e965e826a7c8189f37d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "992e1695c8bcacd0bc91",
+    "url": "/static/js/2.63107de9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.63107de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e965e826a7c8189f37d",
+    "url": "/static/js/main.dc5a9455.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
